cf20c4f95b8e3f1f4caf56a4b7baaed254ba3471
davidm-g <davidmgoncalves.pt@gmail.com>
2024-04-15 15:56:35 +0100

    Fixed logout redirect

--- action_logout.php
+++ action_logout.php
@@ -2,5 +2,5 @@
 
   session_start();                                         // starts the session
   session_destroy();
-  header('Location: ' . $_SERVER['HTTP_REFERER']);         // redirect to the page we came from
+  header('Location: index.php');         // redirect to the page we came from
 ?>

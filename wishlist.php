077875b4c5e03f1d5215f3e942ebcc6dff64d352
davidm-g <davidmgoncalves.pt@gmail.com>
2024-04-21 22:34:12 +0100

    Modified main h1

--- wishlist.php
+++ wishlist.php
@@ -10,7 +10,7 @@
     output_header();
     output_categories($db, $cats);
 ?>
-    <h1><?=$_SESSION['username']?>'s Wishlist</h1>
+    <h1><?=$_SESSION['username']?>'s Wishlist <i class="fa-solid fa-heart" style="color: #f07070;"></i></h1>
     <aside id="random_items">
         <?php foreach ($items as $item) { 
             if(isOnWishlist($db,$item['id'],$_SESSION['username']))
